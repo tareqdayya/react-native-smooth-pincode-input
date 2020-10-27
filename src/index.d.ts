98ddc9db5c0ed792f66bf09f0e73fb0f00720c1c
Tareq Dayya <tareqdayya@gmail.com>
2020-10-27 21:04:48 +0200

    added type file to repo

---
+++ src/index.d.ts
@@ -0,0 +1,44 @@
+import { StyleProp, TextInputProps, TextStyle, ViewStyle } from 'react-native';
+import React = require('react');
+
+type Props = {
+  value: string,
+  codeLength: number,
+  cellSize?: number,
+  cellSpacing?: number,
+
+  placeholder?: string | JSX.Element,
+  mask?: string | JSX.Element,
+  maskDelay?: number,
+  password?: boolean,
+
+  autoFocus?: boolean,
+
+  restrictToNumbers?: boolean,
+
+  containerStyle?: StyleProp<ViewStyle>,
+
+  cellStyle?: StyleProp<ViewStyle>,
+  cellStyleFocused?: StyleProp<ViewStyle>,
+  cellStyleFilled?: StyleProp<ViewStyle>,
+
+  textStyle?: StyleProp<TextStyle>,
+  textStyleFocused?: StyleProp<TextStyle>
+
+  animated?: boolean,
+  animationFocused?: string | object,
+
+  onFulfill?: Function,
+  onChangeText?: Function,
+  onBackspace?: Function,
+  onTextChange?: Function,
+  testID?: any,
+  onFocus?: Function,
+  onBlur?: Function,
+  keyboardType?: string,
+  editable?: boolean,
+  inputProps?: TextInputProps,
+};
+
+export default class SmoothPinCodeInput extends React.Component<Props, any> {
+}
